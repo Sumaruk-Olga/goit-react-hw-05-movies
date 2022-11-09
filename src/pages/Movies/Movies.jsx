d6c78bdb37ada5f142816213782cce62d12aafae
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-11-09 22:06:47 +0200

    add react-icons and yup

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,14 +1,45 @@
+import { Field, Form, Formik } from "formik";
+import { FcSearch } from "react-icons/fc";
 import { Link, useLocation } from "react-router-dom";
 
 const Movies = () => {
     const location = useLocation();
     return <div>
         <p>Movies searchbar</p>
+        <Formik>
+            <Form>
+                <Field />
+                <button type="submit">
+                    <FcSearch/>
+                </button>
+            </Form>
+        </Formik>
         <p>list of films after fetch</p>
         <Link to="movieId" state={{ from: location }}>film</Link>
     </div>
 };
 export default Movies;
 
-//замість "movieId" треба буде вставляти динамічно 
+//замість "movieId" треба буде вставляти динамічно
 // { ` &{qwe} ` }
+
+// {/* <Formik
+//             initialValues={{ searchValue: '' }}
+//             onSubmit={(values, {resetForm}) => {
+//                 onSubmit(values);
+//                 resetForm();
+//     }}>
+//     <StyledForm>
+//         <SearchButton type="submit">
+//             <FcSearch/>
+//         </SearchButton>
+
+//     <StyledField
+//         type="text"
+//         name="searchValue"
+//         autoComplete="off"
+//         autoFocus
+//         placeholder="Search images and photos"
+//     />
+//     </StyledForm>
+// </Formik> */}
