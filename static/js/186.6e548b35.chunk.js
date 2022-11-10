3a8a5d754eccb40148d53842b89a0df911e88a0c
Sumaruk-Olga <91608474+Sumaruk-Olga@users.noreply.github.com>
2022-11-10 21:19:10 +0000

    Deploying to gh-pages from @ Sumaruk-Olga/goit-react-hw-05-movies@a1cd1a4835b4ed8ccbd581226f11328569e0850a 🚀

---
+++ static/js/186.6e548b35.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,t,i){i.r(t);var s=i(7689),n=i(184);t.default=function(){var e=(0,s.TH)();return console.log("locationin reviews",e.state.state),(0,n.jsx)("div",{children:" Reviews info "})}}}]);
+//# sourceMappingURL=186.6e548b35.chunk.js.map
