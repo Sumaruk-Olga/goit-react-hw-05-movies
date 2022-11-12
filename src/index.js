09b2537edbd7de8ad7a9de0d211e9acbdc634d12
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-11-12 23:13:38 +0200

    return strict mode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { theme } from 'utiles/theme';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  //  </React.StrictMode> 
+  </React.StrictMode> 
 );
